8da7aa4e162a2ae5510cc16dedc01c3bc6c2e15b
Veselin Dimitrov <75318134+Veselin-rbots@users.noreply.github.com>
2024-01-31 21:21:25 +0200

    Add files via upload

---
+++ ABSTRACT.py
@@ -0,0 +1,11 @@
+def calculate_area_of_rectangle(side_a, side_b):
+
+    area = side_a * side_b
+
+    return area
+
+side_1 = int(input())
+side_2 = int(input())
+
+print(calculate_area_of_rectangle(side_1, side_2))
+
